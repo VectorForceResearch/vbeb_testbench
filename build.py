22a5d8a0e8865c2e149d7a1779f1e83b03c1f2a3
rossh <rossh@alleninstitute.org>
2018-01-19 07:58:25 -0800

    fixed typo in build.py preventing build depdenecies from aibspi from being recognized.

--- build.py
+++ build.py
@@ -89,7 +89,8 @@ def initialize(project):
     project.set_property('dir_source_pytest_python', "tests")
 
     project.install_dependencies_index_url = 'http://aibspi:3141/aibs/dev'
-    project.nstall_dependencies_insecure_installation = ['aibspi']
+    project.install_dependencies_insecure_installation = ['aibspi']
+
     # documentation
     project.set_property('dir_docs', 'docs')
     project.set_property('sphinx_config_path', 'docs/')
