92199d3487822aae3bc0541432fcf0a38e32cdfd
rossh <rossh@alleninstitute.org>
2018-01-24 11:17:16 -0800

    found undodumented pybuilder feature that fixes the setup.py issues for real.

--- src/visual_behavior/__init__.py
+++ src/visual_behavior/__init__.py
@@ -11,7 +11,7 @@ import yaml
 
 __author__ = """Ross Hytnen"""
 __email__ = 'rossh@alleninstitute.org'
-__version__ = '0.3.16'
+__version__ = '0.3.17'
 
 if sys.version_info.major == 2:
     class FileNotFoundError(Exception):
